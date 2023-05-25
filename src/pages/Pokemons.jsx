8e3e8d737f555152763a8a5df7186eba5fd42369
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2023-05-25 20:44:20 -0300

    Quarto Commit

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -17,7 +17,7 @@ export const Pokemons = ({ setPokemonData }) => {
 
   const getPokemons = () => {
     var endpoints = [];
-    for (var i = 1; i < 50; i++){
+    for (var i = 1; i < 150; i++){
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
     }
     axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
